024479f5cf721e48d0c3a7449635f1c8e975a25f
Under <matheusmarianoe@gmail.com>
2023-03-31 16:15:48 -0300

    Added moveable menu, dark mode storage saving, all subpages

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+let darkMode = localStorage.getItem("dark-mode");
+
 function changeThemes() {
-  document.body.classList.toggle("dark");
+  darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "disabled") {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("dark-mode", "enabled");
+  } else {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("dark-mode", "disabled");
+  }
+
   }
   
- function moveToSubtitles(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth",});
+  window.onload = function themeChecker() {
+    if (darkMode === "enabled") {
+      changeThemes();
+      localStorage.setItem("dark-mode", "enabled");
+    }
   }
+ function moveToSubtitles(id) {
+  document.getElementById(id).scrollIntoView({  block: "center", behavior: "smooth",});
+  }
+
+var ScrollFunction = function() {
+   var y = window.scrollY;
+   if (y >= 200) {
+    document.body.classList.add("show");
+    document.body.classList.remove("hide");
+   } else {
+    document.body.classList.add("hide");
+    document.body.classList.remove("show");
+   }
+ };
+window.addEventListener("scroll", ScrollFunction);
